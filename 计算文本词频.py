b8480d329dd155ca0741f38e1781d33a1c00029c
Yishiyork <3051176425@qq.com>
2023-11-09 21:11:06 +0800

    使用pandas保存文件；增加多进程调用

--- 计算文本词频.py
+++ 计算文本词频.py
@@ -1,6 +1,8 @@
 import os
 import re
 import pandas as pd
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from pathlib import Path
 '''
 输入：文本文件夹、关键词文本
 
@@ -11,30 +13,41 @@ import pandas as pd
 '''
 
 
-def countWordFrequency(folder_path, dict_path):
+def count_keyword_frequency(filepath, keywords):
+    with open(filepath, "r", encoding="utf-8") as f:
+        content = f.read()
+    count = {keyword: content.count(keyword) for keyword in keywords}
+    count['文件名'] = Path(filepath).name
+    return count
 
+
+def countWordFrequency(folder_path, dict_path):
     # 读取词典
     with open(dict_path, "r", encoding="utf-8") as f:
         keywords = [line.strip() for line in f.readlines()]
         keywords = [re.sub(r'\t.*', r'', keyword) for keyword in keywords]
         keywords = [re.sub(r'\nTopic.*', r'', keyword) for keyword in keywords]
 
-    # 遍历文件夹，统计各个关键词出现的次数
-    data = {}
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".txt"):
-            filepath = os.path.join(folder_path, filename)
-            with open(filepath, "r", encoding="utf-8") as f:
-                content = f.read()
-                count = []
-                for keyword in keywords:
-                    count.append(content.count(keyword))
-                data[filename] = count
+    # 获取所有txt文件的路径
+    files = [os.path.join(folder_path, filename) for filename in os.listdir(
+        folder_path) if filename.endswith(".txt")]
+    file_count = len(files)
+    print(f"Total files to process: {file_count}")
 
-    # 将结果保存为Excel表格
-    df = pd.DataFrame.from_dict(data, orient="index", columns=keywords)
-    df.index.name = "文件名"
-    df.to_excel(dict_path + "_result.xlsx")
+    # 创建一个进程池，并行处理文件
+    results = []
+    with ProcessPoolExecutor() as executor:
+        future_to_file = {executor.submit(
+            count_keyword_frequency, file, keywords): file for file in files}
+        for future in as_completed(future_to_file):
+            res = future.result()
+            results.append(res)
+            print(f"Files remaining: {file_count - len(results)}")
+
+    # 将结果保存为CSV表格
+    df = pd.DataFrame(results)
+    csv_path = dict_path + "_result.csv"
+    df.to_csv(csv_path, index=False, encoding="utf-8-sig")
 
     print("完成！！")
     return df
@@ -43,8 +56,8 @@ def countWordFrequency(folder_path, dict_path):
 def main():
 
     # 输入文件夹路径和词典路径
-    folder_path = input("请输入文本文件夹：")
-    dict_path = input("请输入包含关键词的文本，每行为一个关键词：")
+    folder_path = r"E:\Source_for_sale\A股年报 PDF+TXT\A股年报TXT [56080份18.8GB]"
+    dict_path = r"D:\ZZZMydocument\Academic_1101\230404_管理科学投稿\数据 tidy\[require]\数字化转型\许为宾\许为宾.txt"
     countWordFrequency(folder_path, dict_path)
 
 
